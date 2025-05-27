e4d3b3d12f442ea0f3878072b7438f93ddd63cd4
4lbertZer0x <albertalexanderflores30@gmail.com>
2025-05-26 20:54:27 -0400

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import path from "path";
+import router from './routes';
+import dotenv from 'dotenv';
+
+// Cargar variables de entorno
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configuración del motor de vistas EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+// Middleware para procesar datos del formulario
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, "../")));
+
+// Configurar trust proxy para obtener la IP real del cliente
+app.set('trust proxy', true);
+
+// Integrar las rutas
+app.use('/', router);
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.render("index", { 
+    GA_MEASUREMENT_ID: process.env.GA_MEASUREMENT_ID,
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
